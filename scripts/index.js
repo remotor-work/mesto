25d10dd0bc2aa517b956fd81f35dd9c5315258bd
Igor Savin <remotor-work@yandex.ru>
2022-04-17 18:14:28 -0400

    master 2

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,16 @@ const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
 
 const profileInfo = document.querySelector('.profile__info');
-const profileName = profileInfo.querySelector('.profile__name').textContent;
-const profileOccupation = profileInfo.querySelector('.profile__occupation').textContent;
+const profileName = profileInfo.querySelector('.profile__name');
+const profileOccupation = profileInfo.querySelector('.profile__occupation');
 
-const popupContainer = popup.querySelector('.popup__container');
+const popupForm = popup.querySelector('.popup__form');
 
 function popupOpenToggle() {
-    popup.classList.toggle('popup_is-opened');
+    popup.classList.toggle('popup_opened');
 
-    popup.querySelector('.popup__name').setAttribute('value', profileName);
-    popup.querySelector('.popup__job').setAttribute('value', profileOccupation);
-}
-
-function popupOverlayClickHandler(evt) {
-    if (evt.target === evt.currentTarget) {
-        popupOpenToggle();   
-    }
+    popup.querySelector('.popup__name').value = profileName.textContent;
+    popup.querySelector('.popup__job').value = profileOccupation.textContent;
 }
 
 function formSubmitHandler (evt) {
@@ -26,9 +20,11 @@ function formSubmitHandler (evt) {
 
     profileInfo.querySelector('.profile__name').textContent = popup.querySelector('.popup__name').value;
     profileInfo.querySelector('.profile__occupation').textContent = popup.querySelector('.popup__job').value;
+    popup.classList.remove('popup_opened');
 }
 
 openPopupButton.addEventListener('click', popupOpenToggle);
+
 popupCloseButton.addEventListener('click', popupOpenToggle);
-popup.addEventListener('click', popupOverlayClickHandler);
-popupContainer.addEventListener('submit', formSubmitHandler); 
+
+popupForm.addEventListener('submit', formSubmitHandler); 
