5f6b3f6c4074cb7975dab993e905fbfd57fe6b20
Igor Savin <remotor-work@yandex.ru>
2022-04-19 19:28:19 -0400

    master 3

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@ const openPopupButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
 
-const profileInfo = document.querySelector('.profile__info');
-const profileName = profileInfo.querySelector('.profile__name');
-const profileOccupation = profileInfo.querySelector('.profile__occupation');
+const profile = document.querySelector('.profile');
+const profileName = profile.querySelector('.profile__name');
+const profileOccupation = profile.querySelector('.profile__occupation');
 
 const popupForm = popup.querySelector('.popup__form');
 
@@ -18,8 +18,8 @@ function popupOpenToggle() {
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
-    profileInfo.querySelector('.profile__name').textContent = popup.querySelector('.popup__name').value;
-    profileInfo.querySelector('.profile__occupation').textContent = popup.querySelector('.popup__job').value;
+    profile.querySelector('.profile__name').textContent = popup.querySelector('.popup__name').value;
+    profile.querySelector('.profile__occupation').textContent = popup.querySelector('.popup__job').value;
     popup.classList.remove('popup_opened');
 }
 
